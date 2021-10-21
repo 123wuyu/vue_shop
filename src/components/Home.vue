e3a8c5a96bf56012d02c6dfee967bf53a02f2958
123wuyu <350197427@qq.com>
2021-10-21 22:05:28 +0800

    侧边栏子路由

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,130 @@
 <template>
-  <div>
-    home主页
-    <el-button @click="logout">退出</el-button>
-  </div>
+  <el-container>
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" />
+        <span>电商管理后台</span>
+      </div>
+      <el-button @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle" @click="toggleCollapse">|||</div>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+        >
+          <el-submenu :index="item.id" v-for="item in menulist" :key="item.id">
+            <template slot="title">
+              <i :class="iconObj[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ item.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      menulist: [],
+      iconObj: {
+        125: 'iconfont icon-users',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      isCollapse: false
+    }
+  },
+  created() {
+    this.getMenuList()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      console.log(res)
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-header {
+  background: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+
+.el-main {
+  background: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle {
+  background-color: #4a5064;
+  line-height: 24px;
+  color: #fff;
+  font-size: 10px;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
-import { Form, FormItem, Input, Button, Message } from 'element-ui'
+import {
+  Form,
+  FormItem,
+  Input,
+  Button,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem
+} from 'element-ui'
 
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Button)
 Vue.prototype.$message = Message
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
